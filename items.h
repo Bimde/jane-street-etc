6b80affed53a12e369b3882ebc985104a1cafd9d
maxgao18 <maxgao18@yahoo.com>
2019-06-08 13:01:16 -0400

    s

--- items.h
+++ items.h
@@ -1,22 +1,34 @@
 #ifndef ITEMS_H
 #define ITEMS_H
 
-enum Ticker {};
+#include <vector>
+#include <pair>
+
+enum Ticker {BOND, VALBZ, VALE, GS, MS, WFC, XLF};
+
+enum ActionType {BUY, SELL, CONVERT_TO, CONVERT_FROM};
 
 struct Action {
-
+	ActionType actionType;
+	int amount;
 };
 
 struct Order {
-	/* data */
+	int id;
+	Ticker ticker;
+	ActionType actionType;
+	int amount;
+	bool isAcked;
 };
 
 struct Book {
-
+	std::vector<std::pair<int, int>> buyOrders;
+	std::vector<std::pair<int, int>> sellOrders;
 };
 
 struct Holdings {
-
+	Ticker ticker;
+	int amount;
 };
 
 #endif
